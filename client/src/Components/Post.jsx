130dc2e69013695b46a3fb0569a48963b8ba052d
Sarah Abdelfattah <sarah_23s@hotmail.com>
2021-04-26 15:41:56 +0200

    crud operations

--- client/src/Components/Post.jsx
+++ client/src/Components/Post.jsx
@@ -15,9 +15,14 @@ export class Post extends React.Component {
       <div className="post-container">
         <img src={img} alt="stars"></img>
         <div className="info">
-          <h4>{this.state.title}</h4>
+          <h5>{this.state.title}</h5>
           {/* <h6>{this.state.user}</h6> */}
           <p>{this.state.body}</p>
+
+          <div className="btn-area">
+            <button onClick={this.props.handleEdit}>Edit</button>
+            <button onClick={this.props.handleDelete}>Delete</button>
+          </div>
         </div>
       </div>
     );
--- client/src/Pages/Homepage.jsx
+++ client/src/Pages/Homepage.jsx
@@ -2,11 +2,84 @@ import React from "react";
 import Post from "../Components/Post";
 import axios from "axios";
 import Pagination from "@material-ui/lab/Pagination";
+import { InputLabel, Input, TextField } from "@material-ui/core";
+import { Button } from "react-bootstrap";
 export class Homepage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmitAdd = this.handleSubmitAdd.bind(this);
+  }
+
   state = {
     data: [],
     contentToDisplay: [],
     page: 1,
+
+    newTitle: "",
+    newBody: "",
+    tempId: "",
+
+    viewAdd: false,
+    viewModal: false,
+  };
+
+  handleDelete = (id) => {
+    var removeIndex = this.state.data.map((item) => item.id).indexOf(id);
+    ~removeIndex && this.state.data.splice(removeIndex, 1);
+
+    this.setState({ data: this.state.data });
+    this.handlePagination(this.state.page);
+  };
+
+  handleEdit = (id) => {
+    this.setState({ viewModal: true, tempId: id });
+    const foundPost = this.state.data.find((post) => post.id === id);
+    this.setState({ newTitle: foundPost.title, newBody: foundPost.body });
+  };
+
+  handleEditBtn = (id) => {
+    this.setState({ viewModal: true });
+    const foundPost = this.state.data.find((post) => post.id === id);
+    this.setState({ newTitle: foundPost.title, newBody: foundPost.body });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmitAdd = (e) => {
+    let newPost = {
+      userId: Math.floor(Math.random() * 10 + 1),
+      id: parseInt(this.state.data.length) + 1,
+      title: this.state.newTitle,
+      body: this.state.newBody,
+    };
+    let data = this.state.data;
+    data.unshift(newPost);
+
+    this.setState({ data: [] });
+
+    this.setState({ data });
+    this.handlePagination(this.state.page);
+    this.setState({ viewAdd: false });
+  };
+
+  handleSubmitEdit = () => {
+    let data = this.state.data;
+
+    const index = data.findIndex((post) => post.id === this.state.tempId);
+
+    data[index].title = this.state.newTitle;
+    data[index].body = this.state.newBody;
+
+    this.setState({ data: [] });
+
+    this.setState({ data });
+    this.handlePagination(this.state.page);
+    this.setState({ viewModal: false });
   };
 
   handlePagination = async (number) => {
@@ -28,6 +101,7 @@ export class Homepage extends React.Component {
   async componentDidMount() {
     let data = (await axios.get("https://jsonplaceholder.typicode.com/posts"))
       .data;
+    data = data.reverse();
 
     let min = Math.min(20, data.length);
     let contentToDisplay = [];
@@ -43,22 +117,102 @@ export class Homepage extends React.Component {
       <div className="homepage-container">
         <h1>What does it say?</h1>
 
-        <Pagination
-          className="pagination"
-          count={Math.ceil(this.state.data.length / 20)}
-          shape="rounded"
-          hideNextButton="true"
-          hidePrevButton="true"
-          onClick={(event) => {
-            this.handlePagination(event.target.textContent);
-          }}
-        />
+        <div className="controls">
+          <Pagination
+            className="pagination"
+            count={Math.ceil(this.state.data.length / 20)}
+            shape="rounded"
+            hideNextButton="true"
+            hidePrevButton="true"
+            onClick={(event) => {
+              this.handlePagination(event.target.textContent);
+            }}
+          />
+          <button
+            className="addBtn"
+            onClick={() => this.setState({ viewAdd: true })}
+          >
+            Add a new post
+          </button>
+        </div>
+
+        {this.state.viewAdd ? (
+          <div className="newPost">
+            <h4>Add a new Post</h4>
+            <br />
+            <TextField
+              label="Title"
+              fullWidth="true"
+              variant="outlined"
+              name="newTitle"
+              value={this.state.newTitle}
+              onChange={this.handleChange}
+              aria-describedby="my-helper-text"
+            />
+
+            <TextField
+              label="Body"
+              fullWidth="true"
+              multiline="true"
+              name="newBody"
+              value={this.state.newBody}
+              onChange={this.handleChange}
+              aria-describedby="my-helper-text"
+              variant="outlined"
+            />
+
+            <Button className="btn" onClick={this.handleSubmitAdd}>
+              ADD POST
+            </Button>
+          </div>
+        ) : null}
 
         <div className="outer-container">
+          <h4>Posts</h4>
+
           {this.state.contentToDisplay.map((post) => (
-            <Post title={post.id} body={post.body}></Post>
+            <Post
+              title={post.title}
+              body={post.body}
+              key={post.id}
+              handleDelete={() => this.handleDelete(post.id)}
+              handleEdit={() => this.handleEdit(post.id)}
+            ></Post>
           ))}
         </div>
+
+        {this.state.viewModal ? (
+          <div className="edit-modal">
+            <div className="edit-modal-container">
+              <h4>Edit Post</h4>
+              <br />
+              <TextField
+                label="Title"
+                fullWidth="true"
+                variant="outlined"
+                name="newTitle"
+                value={this.state.newTitle}
+                onChange={this.handleChange}
+                aria-describedby="my-helper-text"
+              />
+
+              <TextField
+                label="Body"
+                fullWidth="true"
+                multiline="true"
+                name="newBody"
+                value={this.state.newBody}
+                onChange={this.handleChange}
+                aria-describedby="my-helper-text"
+                variant="outlined"
+              />
+
+              <Button className="btn" onClick={this.handleSubmitEdit}>
+                EDIT POST
+              </Button>
+            </div>
+          </div>
+        ) : null}
       </div>
     );
   }
